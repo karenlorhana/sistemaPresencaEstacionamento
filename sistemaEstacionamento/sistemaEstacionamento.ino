2711bfe6b7a42b591997b7bef6e920984ac3747a
Karen Lorhana <karen10lorhana@gmail.com>
2019-11-21 20:02:21 -0300

    Create sistemaEstacionamento.ino
    atualizado

---
+++ sistemaEstacionamento/sistemaEstacionamento.ino
@@ -0,0 +1,34 @@
+#include "LiquidCrystal.h"
+LiquidCrystal lcd(12, 11,5, 4, 3, 2);
+
+void setup() {
+
+  lcd.begin(16, 2);
+
+  Serial.begin(9600);
+  lcd.setCursor(0,0);
+  //lcd.print("BEM VINDO");
+  pinMode(A5, INPUT);
+  pinMode(7, OUTPUT); //ledVermelho
+  pinMode(10, OUTPUT); //ledVerde
+}
+
+void loop() {
+
+  int vaga = analogRead(A5);
+  Serial.println(vaga);
+  if(vaga > 950) {
+    digitalWrite(10,LOW);
+    digitalWrite(7,HIGH);
+    //lcd.setCursor(0,1);
+    lcd.write("1");
+  }
+  else {
+    digitalWrite(10,HIGH);
+    digitalWrite(7,LOW);
+    //lcd.setCursor(0,1);
+    lcd.write("0");
+  }
+
+}
+ 
